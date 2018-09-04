a523c9ef3e09ad6847c34305d92c6367c485eb71
Akshat <akshat.dev.zero@gmail.com>
2018-09-04 10:53:07 +0530

    Issues fixed as defined by Hacken Smart Contract Audit.

--- contracts/TrueDeckToken.sol
+++ contracts/TrueDeckToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.23;
+pragma solidity 0.4.23;
 
 /**
  * @title SafeMath
@@ -6,70 +6,78 @@ pragma solidity ^0.4.23;
  */
 library SafeMath {
 
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-        if (a == 0) {
+
+    /**
+    * @dev Multiplies two numbers, reverts on overflow.
+    */
+    function mul(uint256 _a, uint256 _b) internal pure returns (uint256) {
+        if (_a == 0) {
             return 0;
         }
-        c = a * b;
-        assert(c / a == b);
+        uint256 c = _a * _b;
+        require(c / _a == _b);
+
         return c;
     }
 
     /**
-    * @dev Integer division of two numbers, truncating the quotient.
+    * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
     */
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        // uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return a / b;
+    function div(uint256 _a, uint256 _b) internal pure returns (uint256) {
+        require(_b > 0); // Solidity only automatically asserts when dividing by 0
+        uint256 c = _a / _b;
+        // assert(_a == _b * c + _a % _b); // There is no case in which this doesn't hold
+
+        return c;
     }
 
     /**
-    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
     */
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
-        return a - b;
+    function sub(uint256 _a, uint256 _b) internal pure returns (uint256) {
+        require(_b <= _a);
+        uint256 c = _a - _b;
+
+        return c;
     }
 
     /**
     * @dev Adds two numbers, throws on overflow.
     */
-    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-        c = a + b;
-        assert(c >= a);
+    function add(uint256 _a, uint256 _b) internal pure returns (uint256) {
+        uint256 c = _a + _b;
+        require(c >= _a);
+
         return c;
     }
-}
-
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
-contract ERC20Basic {
-    function totalSupply() public view returns (uint256);
-    function balanceOf(address who) public view returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
-    event Transfer(
-        address indexed from,
-        address indexed to,
-        uint256 value
-    );
 }
 
 /**
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
-contract ERC20 is ERC20Basic {
-    function allowance(address owner, address spender) public view returns (uint256);
-    function transferFrom(address from, address to, uint256 value) public returns (bool);
-    function approve(address spender, uint256 value) public returns (bool);
+contract ERC20 {
+    function totalSupply() public view returns (uint256);
+
+    function balanceOf(address _who) public view returns (uint256);
+
+    function allowance(address _owner, address _spender)
+        public view returns (uint256);
+
+    function transfer(address _to, uint256 _value) public returns (bool);
+
+    function approve(address _spender, uint256 _value)
+        public returns (bool);
+
+    function transferFrom(address _from, address _to, uint256 _value)
+        public returns (bool);
+
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 value
+    );
+
     event Approval(
         address indexed owner,
         address indexed spender,
@@ -88,11 +96,11 @@ contract TrueDeckToken is ERC20 {
     string public constant symbol = "TDP";
     uint8 public constant decimals = 18;
 
-    mapping (address => uint256) balances;
+    mapping (address => uint256) private balances_;
 
-    mapping (address => mapping (address => uint256)) internal allowed;
+    mapping (address => mapping (address => uint256)) private allowed_;
 
-    uint256 totalSupply_;
+    uint256 private totalSupply_;
 
     /**
     * @dev Initial supply of TDP tokens.
@@ -101,7 +109,7 @@ contract TrueDeckToken is ERC20 {
     uint256 public INITIAL_SUPPLY = 200000000 * 10 ** uint256(decimals);
 
     constructor() public {
-        balances[msg.sender] = INITIAL_SUPPLY;
+        balances_[msg.sender] = INITIAL_SUPPLY;
         totalSupply_ = INITIAL_SUPPLY;
     }
 
@@ -121,12 +129,11 @@ contract TrueDeckToken is ERC20 {
     * @param _value The amount to be transferred.
     */
     function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_value <= balances_[msg.sender]);
         require(_to != address(0));
 
-        require(_value <= balances[msg.sender]);
-
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
+        balances_[msg.sender] = balances_[msg.sender].sub(_value);
+        balances_[_to] = balances_[_to].add(_value);
         emit Transfer(msg.sender, _to, _value);
 
         return true;
@@ -140,13 +147,13 @@ contract TrueDeckToken is ERC20 {
      * @param _value uint256 the amount of tokens to be transferred
      */
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_value <= balances_[_from]);
+        require(_value <= allowed_[_from][msg.sender]);
         require(_to != address(0));
-        require(_value <= balances[_from]);
-        require(_value <= allowed[_from][msg.sender]);
 
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        balances_[_from] = balances_[_from].sub(_value);
+        balances_[_to] = balances_[_to].add(_value);
+        allowed_[_from][msg.sender] = allowed_[_from][msg.sender].sub(_value);
         emit Transfer(_from, _to, _value);
 
         return true;
@@ -163,8 +170,7 @@ contract TrueDeckToken is ERC20 {
      * @param _value The amount of tokens to be spent.
      */
     function approve(address _spender, uint256 _value) public returns (bool) {
-        require(_spender != address(0));
-        allowed[msg.sender][_spender] = _value;
+        allowed_[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
@@ -176,13 +182,13 @@ contract TrueDeckToken is ERC20 {
      * @return A uint256 specifying the amount of tokens still available for the spender.
      */
     function allowance(address _owner, address _spender) public view returns (uint256) {
-        return allowed[_owner][_spender];
+        return allowed_[_owner][_spender];
     }
 
     /**
      * @dev Increase the amount of tokens that an owner allowed to a spender.
      *
-     * approve should be called when allowed[_spender] == 0. To increment
+     * approve should be called when allowed_[_spender] == 0. To increment
      * allowed value is better to use this function to avoid 2 calls (and wait until
      * the first transaction is mined)
      * From MonolithDAO Token.sol
@@ -190,16 +196,15 @@ contract TrueDeckToken is ERC20 {
      * @param _addedValue The amount of tokens to increase the allowance by.
      */
     function increaseApproval(address _spender, uint256 _addedValue) public returns (bool) {
-        require(_spender != address(0));
-        allowed[msg.sender][_spender] = (allowed[msg.sender][_spender].add(_addedValue));
-        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        allowed_[msg.sender][_spender] = (allowed_[msg.sender][_spender].add(_addedValue));
+        emit Approval(msg.sender, _spender, allowed_[msg.sender][_spender]);
         return true;
     }
 
     /**
      * @dev Decrease the amount of tokens that an owner allowed to a spender.
      *
-     * approve should be called when allowed[_spender] == 0. To decrement
+     * approve should be called when allowed_[_spender] == 0. To decrement
      * allowed value is better to use this function to avoid 2 calls (and wait until
      * the first transaction is mined)
      * From MonolithDAO Token.sol
@@ -207,14 +212,13 @@ contract TrueDeckToken is ERC20 {
      * @param _subtractedValue The amount of tokens to decrease the allowance by.
      */
     function decreaseApproval(address _spender, uint256 _subtractedValue) public returns (bool) {
-        require(_spender != address(0));
-        uint256 oldValue = allowed[msg.sender][_spender];
-        if (_subtractedValue > oldValue) {
-            allowed[msg.sender][_spender] = 0;
+        uint256 oldValue = allowed_[msg.sender][_spender];
+        if (_subtractedValue >= oldValue) {
+            allowed_[msg.sender][_spender] = 0;
         } else {
-            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+            allowed_[msg.sender][_spender] = oldValue.sub(_subtractedValue);
         }
-        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        emit Approval(msg.sender, _spender, allowed_[msg.sender][_spender]);
         return true;
     }
 
@@ -224,6 +228,6 @@ contract TrueDeckToken is ERC20 {
     * @return An uint256 representing the amount owned by the passed address.
     */
     function balanceOf(address _owner) public view returns (uint256) {
-        return balances[_owner];
+        return balances_[_owner];
     }
 }
